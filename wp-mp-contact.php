fa418ba0471deb94655864132a53da9e5a8a96a9
Andrew Shankie <andrew@andrewshankie.com>
2014-10-21 10:55:01 +0100

    Adds initial WP plugin text

--- wp-mp-contact.php
+++ wp-mp-contact.php
@@ -1,11 +1,31 @@
 <?php
-
 /**
-*
-* WP-MP-Contact
-*
-* Plugin for emailing a pre-defined message to your UK Member of Parliament
-*
-* @version 0.1
-*
-**/
+ * Plugin Name: WP-MP-Contaact
+ * Plugin URI: https://wordpress.org/plugins/wp-mp-contact
+ * Description: Gravity Forms extension for UK Member of Parliament email campaigns
+ * Version: 0.0.1
+ * Author: Proper Design
+ * Author URI: http://properdesign.rs
+ * License: GPL2
+ *
+ * Acknowledgements: Renewable UK (http://www.renewableuk.com/) for funding the development of this plugin
+ */
+
+/*  Copyright 2014  Proper Design  (email : support@properdesign.rs)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
